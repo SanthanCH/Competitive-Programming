ce12d578185ff7d1872b9f673f51ffac77a2028e
SanthanCH <67743711+SanthanCH@users.noreply.github.com>
2021-08-03 23:32:43 +0530

    Create Longest_Common_Prefix.cpp

---
+++ Longest_Common_Prefix/Longest_Common_Prefix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string s=strs[0];
+        for(int i=1;i<strs.size();i++){
+            string ans="";
+            for(int j=0;j<min(s.length(),strs[1].length());j++){
+                if(s[j]==strs[i][j])
+                    ans+=s[j];
+                else
+                   break;
+            }
+            s=ans;
+        }
+        return s;
+    }
+};
