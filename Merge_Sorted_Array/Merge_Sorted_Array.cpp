31df6493dec231aaec00bf85cf9574f63d640ebb
SanthanCH <67743711+SanthanCH@users.noreply.github.com>
2021-09-09 12:03:12 +0530

    Create Merge_Sorted_Array.cpp

---
+++ Merge_Sorted_Array/Merge_Sorted_Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1,j=n-1,last=n+m-1;
+        while(i>=0&&j>=0){
+            if(nums1[i]>=nums2[j])
+                nums1[last--]=nums1[i--];
+            
+            else
+                nums1[last--]=nums2[j--];
+        }
+        
+            while(i>=0)
+              nums1[last--]=nums1[i--];
+        while(j>=0)
+            nums1[last--]=nums2[j--];
+        
+      
+    }
+};
