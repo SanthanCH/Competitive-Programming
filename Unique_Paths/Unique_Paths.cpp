90c053921271da07079b66becab7aab5d44f9927
SanthanCH <67743711+SanthanCH@users.noreply.github.com>
2021-09-30 16:17:21 +0530

    Update Unique_Paths.cpp

--- Unique_Paths/Unique_Paths.cpp
+++ Unique_Paths/Unique_Paths.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-     int dp[n][m];
-        for(int i=0;i<n;i++){
-          for(int j=0;j<m;j++){
-              dp[i][j]=0;
-              if(i==0)
-                  dp[i][j]=1;
-              if(j==0)
-                  dp[i][j]=1;
-          }  
-        }
-        int i,j;
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                    dp[i][j]+=dp[i-1][j];
-                    dp[i][j]+=dp[i][j-1];
-                    
-            }
-        }
-        return dp[n-1][m-1];
+     int arr[m][n],i,j;
+     for(i=0;i<m;i++)
+     {
+         for(j=0;j<n;j++){
+             if(i==0||j==0)
+                 arr[i][j]=1;
+             else{
+                 arr[i][j]=arr[i-1][j]+arr[i][j-1];
+             }
+         }
+     }
+        return arr[m-1][n-1];
     }
 };
